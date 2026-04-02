ddb5bccae73bf3731b01be9693c2c421a9399f11
Scott Luu <scott@boostsecurity.io>
2026-04-02 15:21:00 -0400

    Change OAuth to Bearer token in samples.sh

--- samples.sh
+++ samples.sh
@@ -1,2 +1,3 @@
-curl -H "Authorization: OAuth sm_aat_NMQntgDppMJhYfb1" http://www.example.com
+curl -H "Authorization: Bearer sm_t_NMQntgDppMJhYfb1" http://www.example.com
+
 curl --user name:baedfe2431f958e978fb555b6b04fd32f9a9bfdc4a7e063a7132b1caaa74f294 http://www.example.com
