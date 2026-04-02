ed492ccd830a27a7453e141ea603f73ced762ad4
Scott Luu <scott@boostsecurity.io>
2026-04-02 14:44:47 -0400

    Add sample script for making HTTP requests

---
+++ samples.sh
@@ -0,0 +1,2 @@
+curl -H "Authorization: OAuth sm_aat_NMQntgDppMJhYfb1" http://www.example.com
+curl --user name:baedfe2431f958e978fb555b6b04fd32f9a9bfdc4a7e063a7132b1caaa74f294 http://www.example.com
